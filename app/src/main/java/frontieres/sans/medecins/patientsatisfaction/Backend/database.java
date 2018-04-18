9e99a7a8e61e3ce37c197798fedf592ad60bcd01
Home <omarelkassar8@gmail.com>
2018-04-18 22:42:36 +0200

    dada base

--- app/src/main/java/frontieres/sans/medecins/patientsatisfaction/Backend/database.java
+++ app/src/main/java/frontieres/sans/medecins/patientsatisfaction/Backend/database.java
@@ -2,6 +2,7 @@ package frontieres.sans.medecins.patientsatisfaction.Backend;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,16 +14,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class database extends SQLiteOpenHelper {
 
-    public static final String Database = "answer_table" ;
+    public static final String Database = "ANSWER_TABLE.db" ;
     public static final String Table_Name = "ANSWERS" ;
     public static final String col1_ID_PATIENT = "ID_PATIENT" ;
     public static final String col2_ID_QUESTION = "ID_QUESTION" ;
     public static final String col3_ANSWER = "ANSWER" ;
-   public static final String CREATE_TABLE_SENTIX = "CREATE TABLE " + Table_Name +" ( "+col1_ID_PATIENT + " INTEGER ," +
-            ","+ col2_ID_QUESTION +" INTEGER  ,  " + col3_ANSWER + " TEXT , PRIMARY KEY ("+col1_ID_PATIENT +" , " +col2_ID_QUESTION + ")) ;" ;
 
-  /*  public static final String CREATE_TABLE_SENTIX = "CREATE TABLE ANSWERS (  INTEGER ,ID_QUESTION INTEGER  ,  ANSWER TEXT ," +
-            " PRIMARY KEY (ID_PATIENT , ID_QUESTION)) ;" ;*/
+    //////
+
+
+
+   public static final String CREATE_TABLE_SENTIX = "CREATE TABLE ANSWERS ( ID_PATIENT INTEGER  AUTOINCREMENT,ID_QUESTION INTEGER  , " +
+           " ANSWER TEXT ,PRIMARY KEY (ID_PATIENT , ID_QUESTION)) " ;
 
     public database(Context context) {
         super(context, Database, null, 1);
@@ -32,16 +35,23 @@ public class database extends SQLiteOpenHelper {
     @Override
 
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_SENTIX);
+        db.execSQL("create table " + Table_Name + "(" + col1_ID_PATIENT + " INTEGER ,"+ col2_ID_QUESTION +"   INTEGER , " +
+                 col3_ANSWER +" TEXT ,PRIMARY KEY ( "+col1_ID_PATIENT+" ," +col2_ID_QUESTION +"))");
     }
+
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
 
-    public boolean insertData(int ID_PATIENT,int ID_QUETION,String ANSWER) {
+    public boolean insertData1(int ID_PATIENT,int ID_QUETION,String ANSWER) {
         SQLiteDatabase db = this.getWritableDatabase();
+        if (db == null) {
+            return false ;
+        }
         ContentValues contentValues = new ContentValues();
         contentValues.put(col1_ID_PATIENT,ID_PATIENT );
         contentValues.put(col2_ID_QUESTION,ID_QUETION );
@@ -50,6 +60,27 @@ public class database extends SQLiteOpenHelper {
         if(result == -1)
             return false;
         else
-            return true ;
+            return true;
     }
+
+
+   public int Next_Id_Patient (){
+       SQLiteDatabase db = this.getWritableDatabase();
+       String selectQuery =  "SELECT MAX("+ col1_ID_PATIENT +") as "+ col1_ID_PATIENT +"  FROM " +  Table_Name  ;
+       Cursor cursor =  db.rawQuery(selectQuery, null);
+       int count = cursor.getCount();
+       if (count == 0) return 0 ;
+       cursor.moveToFirst() ;
+       int an = cursor.getInt( cursor.getColumnIndex(col1_ID_PATIENT));
+       return  an ;
+   }
+
+public boolean delete (int n) {
+    SQLiteDatabase db = this.getWritableDatabase();
+    return db.delete(Table_Name , col1_ID_PATIENT + '=' + n ,null) > 0 ;
+
+}
+
+
+
 }
